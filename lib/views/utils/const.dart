cc121475be78a3837353da5d97d233b4ec05b1eb
haris ardian <harisfirman387@gmail.com>
2024-07-10 19:38:38 +0700

    feat:create const color

---
+++ lib/views/utils/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color mainRed = const Color.fromARGB(255, 205, 12, 12);
+Color blackColor = const Color.fromARGB(255, 15, 15, 19);
+Color whiteColor = const Color(0xffffffff);
+
