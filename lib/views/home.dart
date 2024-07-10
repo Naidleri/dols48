02696877dd15b8aed415d25b1789835766d0106f
haris ardian <harisfirman387@gmail.com>
2024-07-10 19:39:00 +0700

    refactor sctructure

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,18 +8,12 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 47, 44, 44),
-      appBar: AppBar(
-        title: const Text(
-          "Dols 48",
-          style: TextStyle(
-              color: Colors.red, fontWeight: FontWeight.w900, fontSize: 30),
-        ),
-        backgroundColor: const Color.fromARGB(255, 47, 44, 44),
-      ),
-      body: const Text("homepage"),
+      body: Text("homepage"),
     );
+    ;
   }
 }
