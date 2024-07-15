4cbed03086cfe178765f15fe045956c10ddb97a6
haris ardian <harisfirman387@gmail.com>
2024-07-15 12:04:40 +0700

    feat:update ui add message and member

--- lib/views/member.dart
+++ lib/views/member.dart
@@ -1,4 +1,9 @@
+import 'package:dols48/views/fragment/listview.dart';
+import 'package:dols48/views/member/member.dart';
+import 'package:dols48/views/messages/message.dart';
+import 'package:dols48/views/utils/const.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MemberPage extends StatefulWidget {
   const MemberPage({super.key});
@@ -10,9 +15,29 @@ class MemberPage extends StatefulWidget {
 class _MemberPageState extends State<MemberPage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 47, 44, 44),
-      body: Text("member"),
-    );
+        appBar: AppBar(
+          title: Text(
+            "Dols48 DM",
+            style: GoogleFonts.poppins(
+              color: blackColor,
+              fontSize: screenWidth * 0.04,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(3.0),
+                child: Message(),
+              ),
+              Member(),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[50]);
   }
 }
