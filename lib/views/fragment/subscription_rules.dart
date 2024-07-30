d3d7301fde47b8990f31afc7b023dc17c5b6d177
haris ardian <harisfirman387@gmail.com>
2024-07-30 11:01:23 +0700

    update:ui update scrollable

--- lib/views/fragment/subscription_rules.dart
+++ lib/views/fragment/subscription_rules.dart
@@ -1,8 +1,6 @@
-import 'package:dols48/views/fragment/button.dart';
-import 'package:flutter/material.dart';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:dols48/views/fragment/button.dart';
 
 class SubscriptionRules extends StatelessWidget {
   final String member;
@@ -17,8 +15,13 @@ class SubscriptionRules extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
       width: screenWidth,
+      constraints: BoxConstraints(
+        maxHeight: screenHeight * 0.75, // Maksimal tinggi 3/4 dari layar
+      ),
       padding: const EdgeInsets.all(16.0),
       decoration: const BoxDecoration(
         color: Colors.white,
@@ -26,36 +29,46 @@ class SubscriptionRules extends StatelessWidget {
           top: Radius.circular(20),
         ),
       ),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              child: const CircleAvatar(
-                radius: 50,
+      child: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    child: const CircleAvatar(
+                      radius: 30,
+                    ),
+                  ),
+                  Text(
+                    member,
+                    style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    detail,
+                    style: GoogleFonts.poppins(fontSize: 16),
+                  ),
+                  SizedBox(height: 16),
+                ],
               ),
             ),
-            Text(
-              member,
-              style: GoogleFonts.poppins(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: PublicButton(
+              label: "Subscribe",
+              onPressed: () {},
             ),
-            SizedBox(height: 8),
-            Text(
-              detail,
-              style: GoogleFonts.poppins(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: PublicButton(label: "Subscribe", onPressed: () {})
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
